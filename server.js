df6022e159b65246922505d0597e660e07c0be47
anthonyvigz <anthonyvigz@gmail.com>
2019-08-13 20:16:16 -0400

    created logger middleware

--- server.js
+++ server.js
@@ -7,16 +7,26 @@ server.use(express.json());
 /// implementing routers
 
 const usersRouter = require('./users/userRouter.js');
-const postsRouter = require('./posts/postRouter.js')
+const postsRouter = require('./posts/postRouter.js');
 
-server.get('/', (req, res) => {
+// routing
+
+server.use('/api/users', usersRouter);
+server.use('/api/posts', postsRouter);
+
+// main get request to server
+
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
 
 function logger(req, res, next) {
+  console.log
+    (`You made a ${req.method} request to ${req.url} at ${Date.now()}`);
 
+  next();
 };
 
 module.exports = server;
