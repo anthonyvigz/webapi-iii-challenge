3611f03988e8a3cb5e3d60bf1f9d67f9b3335ec3
anthonyvigz <anthonyvigz@gmail.com>
2019-08-13 20:07:59 -0400

    implented express into routers and router into server

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
+/// implementing routers
+
+const usersRouter = require('./users/userRouter.js');
+const postsRouter = require('./posts/postRouter.js')
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
