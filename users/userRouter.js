782b4ca4d59b1cc18b83eaf412527fb99d4e01e4
anthonyvigz <anthonyvigz@gmail.com>
2019-08-13 20:38:40 -0400

    created validate user id middleware

--- users/userRouter.js
+++ users/userRouter.js
@@ -2,8 +2,28 @@ const express = require('express');
 
 const router = express.Router();
 
+// importing users from database
+
+const Users = require('./userDb.js')
+
+
+// post a user to the server
+
 router.post('/', (req, res) => {
 
+    const user = req.body;
+
+    Users.insert(user)
+        .then(users => {
+            if(!user.name) {
+                res.status(400).json({ message: 'Please provide a user.'})
+            } else {
+                res.status(201).json(users)
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'There was an error saving a user.'})
+        })
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -33,7 +53,20 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
+    const { id } = req.params;
 
+    Users.getById(id)
+        .then(user => {
+            if(user) {
+                req.user = user;
+                next();
+            } else {
+                res.status(404).json({ message: 'Invalid ID: User ID does not exist.'})
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'Failed to process ID request' })
+        })
 };
 
 function validateUser(req, res, next) {
